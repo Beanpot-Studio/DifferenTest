4bf44e142ee658afbe145404acc936ac12ecc3fa
Jen Looper <jen.looper@gmail.com>
2025-04-24 23:54:54 -0400

    paths for curricula

--- src/components/CurriculumCard.vue
+++ src/components/CurriculumCard.vue
@@ -35,7 +35,7 @@
                   <img :src="quiz.badgeImage" class="w-10 h-10" />
                 </td>
                 <td class="px-4 py-2 whitespace-nowrap text-sm text-gray-900">
-                  <a :href="`${baseUrl}${basePath}/${curriculum.id}/quiz/${quiz.id}`" class="text-blue-600 hover:text-blue-800">
+                  <a :href="`${baseUrl}/classes/${curriculum.id}/quiz/${quiz.id}`" class="text-blue-600 hover:text-blue-800">
                     {{ quiz.title }}
                   </a>
                 </td>
