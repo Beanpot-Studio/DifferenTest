3772e92f637c39ac2c1f79ca0289c88e5787570d
Jen Looper <jen.looper@gmail.com>
2025-04-27 01:13:53 -0400

    removing contact form and server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
-  experimental: {
-    session: true
-  },
   integrations: [
     tailwind(),
     vue(),
