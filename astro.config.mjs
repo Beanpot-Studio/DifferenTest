eb2976f69a4dca001dda61713b0ecd6d473402c4
Jen Looper <jen.looper@gmail.com>
2025-04-28 11:06:59 -0400

    astro upgrade, node sessions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
-import { node } from '@astrojs/node';
+import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   //for contact form
