101da913715af53cfcb18c489e70c959537be362
Jen Looper <jen.looper@gmail.com>
2025-04-28 22:42:06 -0400

    removing mailgun

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly PUBLIC_MAILGUN_API_KEY: string;
-  readonly PUBLIC_MAILGUN_DOMAIN: string;
-  readonly PUBLIC_MAILGUN_CONTACT_EMAIL: string;
 }
 
 interface ImportMeta {
