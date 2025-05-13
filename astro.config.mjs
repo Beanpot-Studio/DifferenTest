99856fad2fc581b09d242d747bbf6f0850ae5fae
Jen Looper <jen.looper@gmail.com>
2025-05-13 00:25:40 -0400

    ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
+import netlify from "@astrojs/netlify";
 //import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
- 
+  adapter: netlify(),
   integrations: [
     tailwind(),
     vue(),
